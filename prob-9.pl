b2bef4e2747558b3b807e722bc2f8aa736d4fb13
Saurabh Verma <saurabhverma@ymail.com>
2012-06-26 12:32:17 +0530

    Added prob 11

--- prob-9.pl
+++ prob-9.pl
@@ -1,3 +1,5 @@
+# this solution is probably fast enough for given input set but it stills requires cleanup since it doesn't take account of duplicates 
+# no strict yet . 
 foreach $i ( 1 .. 499 ) { 
 	foreach $j ( $i .. 499 ) { 
 		$k = 1000 - ( $i + $j ) ; 
@@ -5,7 +7,7 @@ foreach $i ( 1 .. 499 ) {
 		@array = sort {$a <=> $b} @array; 
 #		print "@array\n";
 		if ( ( ($array[0] * $array[0]) + ($array[1]*$array[1]) ) == ( $array[2] * $array[2] ) ) { 
-			print "Pythogon triplet :" ,  $i*$j*$k ;
+			print "Pythogon triplet:" , "$i,$j,$k PRODUCT:" ,  $i*$j*$k , "\n";
 		} 
 	}
 	
